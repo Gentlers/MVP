23ff8d0ca41b528bb319d9e105d55d1e2dae6ef2
Evert Diaz <evertdiazb@gmail.com>
2017-02-25 20:48:47 -0500

    Arreglo en el AJAX para pre-registro

--- public/js/preRegistro.js
+++ public/js/preRegistro.js
@@ -1,15 +1,12 @@
 var working = false;
 $('#botn').on('click', function(e) {
   e.preventDefault();
-  setTimeout(function() {
-    $.ajax({
-    method: "POST",
-    url: "/pre",
-    data: $('#pronto-date-know').serializeArray()
-    })
-    .done(function( msg ) {
-      alert('¡Gracias por pre-registrarte!\nPronto podrás ser parte de la experiencia GENTO.');
-    });
-  }, 3000);
-
+  $.ajax({
+  method: "POST",
+  url: "/pre",
+  data: $('#pronto-date-know').serializeArray()
+  })
+  .done(function( msg ) {
+    alert('¡Gracias por pre-registrarte!\nPronto podrás ser parte de la experiencia GENTO.');
+  });
 });
