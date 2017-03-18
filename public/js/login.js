a2c8f5d9b6b8964a056c3be1ac1608af7d39c419
Evert Diaz <evertdiazb@gmail.com>
2017-03-17 20:01:10 -0500

    Añadida validacion de registro, correo y datos completos

--- public/js/login.js
+++ public/js/login.js
@@ -1,14 +1,20 @@
 var workingRegistro = false;
 var workingLogin = false;
-$('#btnLogin').on('click', function(e) {
+$('#btnLogin').on('click', e => {
   e.preventDefault();
   if (workingLogin) return;
+  var email = $('#formLogin input[name="email"')[0].value
+  var password = $('#formLogin input[name="password"')[0].value
+  if(email == "" || password == "") {
+    alert('Ingresa todos los datos requeridos')
+    break
+  }
   workingLogin = true;
   var $this = $('.login'),
-    $state = $this.find('button > .state');
+  $state = $this.find('button > .state');
   $this.addClass('loading');
   $state.html('Iniciando Sesion...');
-  setTimeout(function() {
+  setTimeout(() => {
     $this.addClass('ok');
     $state.html('!Bienvenido a Gento¡');
     $('.login').submit()
@@ -16,37 +22,39 @@ $('#btnLogin').on('click', function(e) {
 
 });
 
-$('#btnRegistro').on('click', function(e) {
+$('#btnRegistro').on('click', e => {
   e.preventDefault();
   if (workingRegistro) return;
+  var email = $('#formRegistro input[name="email"')[0].value
+  var nombre = $('#formRegistro input[name="nombre"')[0].value
+  var phone = $('#formRegistro input[name="phone"')[0].value
+  var password = $('#formRegistro input[name="password"')[0].value
+  if( email == "" || nombre == "" || phone == "" || password == "") {
+    alert('Ingresa todos los datos requeridos')
+    break
+  }
   workingRegistro = true;
   var $this = $('.registro'),
-    $state = $this.find('button > .state');
+  $state = $this.find('button > .state');
   $this.addClass('loading');
   $state.html('Realizando Registro...');
-  setTimeout(function() {
+  setTimeout(() => {
     $this.addClass('ok');
     $state.html('¡Bienvenido a Gento!');
     $('.registro').submit()
   }, 3000);
 
-});
-
-$('.cambio').on('click', function(e) {
-  alert('hola')
-  $('.login').css('display','none')
-  $('.loginR').css('display','block')
 })
 
-
-$('.fireLogin').on('click', function(e) {
+$('.fireLogin').on('click', e => {
   e.preventDefault()
   $('#formRegistro').css('display','none')
   $('#formLogin').css('display','block')
   $('.fireLogin').css('background','#1AD245')
   $('.fireRegister').css('background','#666')
 })
-$('.fireRegister').on('click', function(e) {
+
+$('.fireRegister').on('click', e => {
   e.preventDefault()
   $('#formLogin').css('display','none')
   $('#formRegistro').css('display','block')
