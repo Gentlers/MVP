597df8872d628c62fefe1b6f86e4d6c4d88f0844
Evert Diaz <evertdiazb@gmail.com>
2017-03-23 15:36:26 -0500

    ESLint public/js/perfil.js

--- public/js/perfil.js
+++ public/js/perfil.js
@@ -1,6 +1,6 @@
-$('.btn-edicion').on('click', function(e) {
-	e.preventDefault();
-	$('#edicion').css("display","block")
-	$this = $('.btn-edicion')
-	$this.css("display","none")
+$('.btn-edicion').on('click', (e) => {
+  e.preventDefault()
+  $('#edicion').css('display', 'block')
+  $this = $('.btn-edicion')
+  $this.css('display', 'none')
 })
