3da812a6ed1dcdc39b2dec4deb98a5c532cf777c
Evert Diaz <evertdiazb@gmail.com>
2017-03-23 15:39:59 -0500

    ESLint public/js/login.js

--- public/js/login.js
+++ public/js/login.js
@@ -1,45 +1,45 @@
-var workingRegistro = false;
-var workingLogin = false;
+var workingRegistro = false
+var workingLogin = false
 $('.login').on('submit', e => {
-  if (workingLogin) return;
-  workingLogin = true;
-  var $this = $('.login'),
-  $state = $this.find('button > .state');
-  $this.addClass('loading');
-  $state.html('Iniciando Sesion...');
+  if (workingLogin) return
+  workingLogin = true
+  var $this = $('.login')
+  var $state = $this.find('button > .state')
+  $this.addClass('loading')
+  $state.html('Iniciando Sesion...')
   setTimeout(() => {
-    $this.addClass('ok');
-    $state.html('!Bienvenido a Gento¡');
+    $this.addClass('ok')
+    $state.html('!Bienvenido a Gento¡')
     $('.login').submit()
   }, 3000)
 })
 
 $('.registro').on('submit', e => {
-  if (workingRegistro) return;
-  workingRegistro = true;
-  var $this = $('.registro'),
-  $state = $this.find('button > .state');
-  $this.addClass('loading');
-  $state.html('Realizando Registro...');
+  if (workingRegistro) return
+  workingRegistro = true
+  var $this = $('.registro')
+  var $state = $this.find('button > .state')
+  $this.addClass('loading')
+  $state.html('Realizando Registro...')
   setTimeout(() => {
-    $this.addClass('ok');
-    $state.html('¡Bienvenido a Gento!');
+    $this.addClass('ok')
+    $state.html('¡Bienvenido a Gento!')
     $('.registro').submit()
   }, 3000)
 })
 
 $('.fireLogin').on('click', e => {
   e.preventDefault()
-  $('#formRegistro').css('display','none')
-  $('#formLogin').css('display','block')
-  $('.fireLogin').css('background','#1AD245')
-  $('.fireRegister').css('background','#666')
+  $('#formRegistro').css('display', 'none')
+  $('#formLogin').css('display', 'block')
+  $('.fireLogin').css('background', '#1AD245')
+  $('.fireRegister').css('background', '#666')
 })
 
 $('.fireRegister').on('click', e => {
   e.preventDefault()
-  $('#formLogin').css('display','none')
-  $('#formRegistro').css('display','block')
-  $('.fireLogin').css('background','#666')
-  $('.fireRegister').css('background','#1AD245')
+  $('#formLogin').css('display', 'none')
+  $('#formRegistro').css('display', 'block')
+  $('.fireLogin').css('background', '#666')
+  $('.fireRegister').css('background', '#1AD245')
 })
