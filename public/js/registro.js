856305b8e44ef5082ce6bb51fb96d7c9b918d01e
Evert Diaz <evertdiazb@gmail.com>
2017-03-23 15:33:22 -0500

    ESLint public/js/registro.js

--- public/js/registro.js
+++ public/js/registro.js
@@ -1,40 +1,35 @@
-$(document).ready(function() {
-   $('select').material_select();
-   $('.carousel.carousel-slider').carousel({full_width: true, height:500});
-   Materialize.updateTextFields();
-   
-   //SCRIPT FOR THE NAVBAR
-   var sizeWidth = $(window).width();
-   $(".button-collapse").sideNav({menuWidth: sizeWidth, draggable: false});
-   $(function() {
-     // grab an element
-     var header = document.getElementById('header-web-gento');
-     // construct an instance of Headroom, passing the element
-     var headroom  = new Headroom(header,{
-       "offset": 260,
-       "tolerance": 5,
-       "classes": {
-         "initial": "animated",
-         "pinned": "slideDown",
-         "unpinned": "slideUp"
-       }
-
-     });
-     // initialise
-     headroom.init();
-   });
-   $(function(){
-     // jQuery methods go here...
-     var btnMenu = $('.button-collapse.icono-button'),
-     iconMenu = $('.material-icons.icono');
-
-     btnMenu.on('click', function (e) {
-       if (iconMenu.text() === "menu") {
-         $(iconMenu).text("close");
-       }
-       else{
-         $(iconMenu).text("menu");
-       }
-     })
-   });
-});
+$(document).ready(() => {
+  $('select').material_select()
+  $('.carousel.carousel-slider').carousel({full_width: true, height: 500})
+  Materialize.updateTextFields()
+  // SCRIPT FOR THE NAVBAR
+  var sizeWidth = $(window).width()
+  $('.button-collapse').sideNav({menuWidth: sizeWidth, draggable: false})
+  $(() => {
+    // grab an element
+    var header = document.getElementById('header-web-gento')
+    // construct an instance of Headroom, passing the element
+    var headroom = new Headroom(header, {
+      'offset': 260,
+      'tolerance': 5,
+      'classes': {
+        'initial': 'animated',
+        'pinned': 'slideDown',
+        'unpinned': 'slideUp'
+      }
+    })
+    // initialise
+    headroom.init()
+  })
+  $(() => {
+    var btnMenu = $('.button-collapse.icono-button')
+    var iconMenu = $('.material-icons.icono')
+    btnMenu.on('click', (e) => {
+      if (iconMenu.text() === 'menu') {
+        $(iconMenu).text('close')
+      } else {
+        $(iconMenu).text('menu')
+      }
+    })
+  })
+})
